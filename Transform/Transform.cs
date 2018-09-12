6e6152bcbcc194256fada7726b64ef227e163478
aman5prakash <amanprakash512@gmail.com>
2018-09-12 16:02:07 +0530

    Assignment 4

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 
 namespace Transform
 {
+    public delegate int operation(int numbers);
     public static class Transform 
     {
-        public static void Map()
+        
+        public static int[] Map(this int[] numbers, operation op)
         {
-            throw new NotImplementedException();
+            int n = numbers.Length;
+            int[] operated = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                operated[i] = op(numbers[i]);
+            }
+            return operated;            
         }
     }
 }
