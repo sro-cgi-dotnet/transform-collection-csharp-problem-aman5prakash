562543ffb36653a38ed290f162f938cd6709a626
aman5prakash <amanprakash512@gmail.com>
2018-09-17 10:04:47 +0530

    comments

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -11,7 +11,11 @@ namespace Transform
         public static int[] Map(this int[] numbers, operation op)
         {
             int n = numbers.Length;
+
+            //Making an arrar of same length of input array
             int[] operated = new int[n];
+
+            // apply lambda expression to each element of input array
             for(int i = 0; i < n; i++)
             {
                 operated[i] = op(numbers[i]);
